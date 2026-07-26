853261748ca66e03d75466940ede003c48313954
Thomas Chandler <140824457+Tomchandla@users.noreply.github.com>
2026-07-26 19:55:14 +1000

    Refactor message creation in issue function

--- pq_blind_signatures/blind-signatures-conservative-rain/src/blind_sig_conservative_rain/issue.rs
+++ pq_blind_signatures/blind-signatures-conservative-rain/src/blind_sig_conservative_rain/issue.rs
@@ -1,6 +1,6 @@
 extern crate rand;
-use super::{NibsLowmc, NonceType, PresignatureType, RecipientPkType, SkType};
-use crate::derive::{NONCE_BYTES};
+use super::{presig_message, NibsLowmc, NonceType, PresignatureType, RecipientPkType, SkType};
+use crate::derive::NONCE_BYTES;
 use rand::Rng;
 
 impl NibsLowmc {
@@ -11,7 +11,7 @@ impl NibsLowmc {
     ) -> (PresignatureType, NonceType) {
         let mut rng = rand::rng();
         let nonce: NonceType = (0..NONCE_BYTES).map(|_| rng.random()).collect();
-        let msg = [pk_r.as_slice(), nonce.as_slice()].concat();
+        let msg = presig_message(pk_r, &nonce);
         let presig = self.mayo.sign_fixed_length_rain(sk, &msg);
         (presig, nonce)
     }
